8585aee4045ae3acb7ab4271d5a502bf8a04733c
chixirra <chixirra@gmail.com>
2026-08-07 03:53:10 +0300

    feat: create development scripts & configure vite for mobile hot reload

--- core/app/vite.config.ts
+++ core/app/vite.config.ts
@@ -1,7 +1,37 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
+import { getLocalIPv4 } from '@isolde/utils';
+
+const isHostMode = process.argv.includes('--host') || process.argv.includes('-h');
+
+const localIp = isHostMode
+  ? process.env.TAURI_DEV_HOST || getLocalIPv4()
+  : 'localhost';
 
 export default defineConfig({
-	plugins: [tailwindcss(), sveltekit()]
+  plugins: [tailwindcss(), sveltekit()],
+  clearScreen: false,
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: isHostMode,
+    hmr: {
+      protocol: 'ws',
+      host: localIp,
+      port: 5173,
+    },
+    watch: {
+      ignored: ['**/src-tauri/**'],
+    },
+  },
+  envPrefix: ['VITE_', 'TAURI_ENV_*'],
+  build: {
+    target:
+      process.env.TAURI_ENV_PLATFORM == 'windows'
+        ? 'chrome105'
+        : 'safari13',
+    minify: !process.env.TAURI_ENV_DEBUG ? 'esbuild' : false,
+    sourcemap: !!process.env.TAURI_ENV_DEBUG,
+  },
 });
