7c009962b8c232826c09ebb7aaaeac639293b071
chixirra <chixirra@gmail.com>
2026-08-11 08:21:40 +0300

    fix: svelte lsp configuration and typescript setup to support $lib path cuz i forgot to

--- core/app/svelte.config.js
+++ core/app/svelte.config.js
@@ -1,22 +1,19 @@
-import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
-	compilerOptions: {
-		runes: ({ filename }) =>
-			filename.split(/[/\\]/).includes('node_modules') ? undefined : true
-	},
-	kit: {
-		adapter: adapter({
-			pages: 'build',
-			assets: 'build',
-			fallback: 'index.html',
-			precompress: false,
-			strict: true
-		})
-	}
+  preprocess: vitePreprocess(),
+  compilerOptions: { runes: true },
+  kit: {
+    adapter: adapter({
+      pages: "build",
+      assets: "build",
+      fallback: "index.html",
+      precompress: false,
+      strict: true,
+    }),
+  },
 };
 
 export default config;
