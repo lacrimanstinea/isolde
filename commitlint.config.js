d6e57260ca328ee9edaab1909e0e52a87afeb638
chixirra <chixirra@gmail.com>
2026-08-11 08:30:29 +0300

    chore: enforce commit name convention

---
+++ commitlint.config.js
@@ -0,0 +1,12 @@
+/**
+ * enforces these commit message conventions
+ *
+ * feat: (new feature)
+ * fix: (bug fix)
+ * docs: (documentation changes)
+ * style: (formatting, missing semi colons, etc.)
+ * refactor: (refactoring code)
+ * test: (adding tests)
+ * chore: (updating build tasks, package manager configs, etc.)
+ */
+export default { extends: ["@commitlint/config-conventional"] };
